545830d1258ab0aa67d86929212b62e0b4fccf2b
moukthik-narayanam <60218627+moukthik-narayanam@users.noreply.github.com>
2021-10-12 10:21:38 +0530

    Step3

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+
 app.set("views",path.join(__dirname, 'views'));
 
 app.set("view engine","ejs");
@@ -20,6 +22,25 @@ app.get('/savings', (req, res) => res.render('account', {account: accounts.savin
 app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
 app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
 app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
+app.get('/transfer',(req,res) => {res.render('transfer');});
+
+app.post('/transfer',(req,res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment',(req,res) => {res.render('payment',{account:accounts.credit});});
+
+app.post('/payment',(req,res) => {
+    accounts.credit.balance = accounts.credit.balance - req.body.amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Completed'});
+});
 
 
 app.listen(3000, () => console.log('Project Running on port 3000!'));
